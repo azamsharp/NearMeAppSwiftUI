744edcd8eba895975aa603055bf8834c23339c49
Mohammad Azam <azamsharp@gmail.com>
2021-05-05 09:09:56 -0500

    Initial Commit

---
+++ NearMe/NearMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearMeApp.swift
+//  NearMe
+//
+//  Created by Mohammad Azam on 5/5/21.
+//
+
+import SwiftUI
+
+@main
+struct NearMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
